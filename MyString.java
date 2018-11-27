961cdd9f55d5c93987ec7cf731f3469bc8223b0d
Ben Shpetner <bshpetner00@stuy.edu>
2018-11-26 19:21:51 -0500

    finished compareTo

--- MyString.java
+++ MyString.java
@@ -1,5 +1,7 @@
-public class MyString implements CharSequence{
+//Ethan helped me brainstorm on planning out the structure of compareTo before I began writing code for it;
+public class MyString implements CharSequence, Comparable<CharSequence>{
 	private char[]data;
+	//the mighty constructor;
 	public MyString(CharSequence s) {
 		int length = s.length();
 		data = new char[length];
@@ -7,9 +9,11 @@ public class MyString implements CharSequence{
 			data[i] = s.charAt(i);
 		} 
 	}
+	//returns the length;
 	public int length() {
 		return data.length;
 	}
+	//returns character at specified index;
 	public char charAt(int i) {
 		try {
 			return data[i];
@@ -18,6 +22,7 @@ public class MyString implements CharSequence{
 			throw new IndexOutOfBoundsException();
 		}
 	}
+	//returns sequence of chars in given MyString from start to end, inclusve and exclusive respectively;
 	public CharSequence subSequence(int start, int end) {
 		String sequence = "";
 		try {
@@ -30,12 +35,51 @@ public class MyString implements CharSequence{
 			throw new IndexOutOfBoundsException();
 		}
 	}
+	//returns a direct string copy of MyString;
 	public String toString() {
 		String s = "";
 		for (int i = 0; i < data.length; i++) {
 			s += data[i];
 		}
 		return s;
+	}
+
+	//compares each sequence to determine which is greater or lesser (if either) in terms of each's character's ASCII values
+	public int compareTo(CharSequence s) {
+		if (s == null) {
+			throw new NullPointerException();
+		}
+		int dis = this.length();
+		int dat = s.length();
+		if (s.toString().equals(this.toString())) {
+			return 0;
+		}
+		if (dis == dat) {
+			for (int i = 0; i < dis; i++) {
+				if (this.charAt(i) != s.charAt(i)) {
+					return this.charAt(i) - s.charAt(i);
+				}
+			}
+			return this.charAt(dis-1) - s.charAt(dis-1);
+		}
+
+		if (dis > dat) {
+			for (int i = 0; i < dat; i++) {
+				if (this.charAt(i) != s.charAt(i)) {
+					return this.charAt(i) - s.charAt(i);
+				}
+			}
+		}
+
+		else {
+			for (int i = 0; i < dis; i++) {
+				if (this.charAt(i) != s.charAt(i)) {
+					return this.charAt(i) - s.charAt(i);
+				}
+			}
+		}
+		//if one string is part of the other string, return the difference in length between this and s
+		return this.length() - s.length();
 	}
 
 }
--- driver.java
+++ driver.java
@@ -1,6 +1,6 @@
 public class driver {
 	public static void main(String[]args) {
-		MyString deeble = new MyString("String");
+		MyString deeble = new MyString("Stringb");
 		System.out.println(deeble.charAt(0));
 		System.out.println(deeble.charAt(1));
 		System.out.println(deeble.charAt(2));
@@ -10,5 +10,10 @@ public class driver {
 		System.out.println(deeble.length());
 		System.out.println(deeble.subSequence(0,6));
 		System.out.println(deeble.toString());
+		System.out.println(deeble.compareTo("Stringa"));
+		System.out.println(deeble.compareTo("Stringc"));
+		System.out.println(deeble.compareTo("Stringb"));
+		System.out.println(deeble.compareTo("Stringba"));
+		System.out.println(deeble.compareTo("String"));
 	}
 }
